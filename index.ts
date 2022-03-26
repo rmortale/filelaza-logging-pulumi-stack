649e6866d5e6f9cc88bf08817def894762e6398b
Antonio Dolce <dolce.antonio@gmail.com>
2022-03-26 14:30:27 +0100

    added event rule

--- index.ts
+++ index.ts
@@ -9,7 +9,24 @@ const log = new aws.cloudwatch.LogGroup(`/aws/events/${prefix}-events-log`, {
     retentionInDays: 7
 });
 
+const s3objCreated = new aws.cloudwatch.EventRule(`${prefix}-s3ObjectCreated`, {
+    description: "Capture each S3 object created event",
+    eventPattern: JSON.stringify({
+        "source": ["aws.s3"],
+        "detail-type": ["Object Created"],
+        "detail": {
+            "bucket": {
+                "name": ["filelaza*"]
+            }
+        }
+    })
+});
 
 
+const s3eventTarget = new aws.cloudwatch.EventTarget(`${prefix}-s3-event-target`, {
+    rule: s3objCreated.name,
+    targetId: "SendToLogS3",
+    arn: log.arn,
+});
 
 export const logGroup = log.id;
